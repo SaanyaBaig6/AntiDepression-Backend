003bba090417aa0c0e5628078e591ae20e2d3d76
saanyabaig6 <saanyabaig6@gmail.com>
2019-12-01 23:33:54 -0600

    config updated

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
     "port": 3000,//process.env.PORT,
-    "mongoUrl": 'mongodb+srv://root:root@cluster0-c6zl6.mongodb.net/AntiDepression?retryWrites=true&w=majority',//process.env.MONGODB_URI,
+    "mongoUrl": '',//process.env.MONGODB_URI,
     "bodyLimit": "100kb"
   }
   
